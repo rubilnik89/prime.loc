d15f28d832028e9ec585aea76509461201b017c4
Roman <rubilnik89@mail.ru>
2017-03-10 14:14:57 +0200

    modify views to show data without tables

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -12,40 +12,58 @@
         <div class="tab-content">
             <div id="home" class="tab-pane fade in active">
                 <div class="col-md-12">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Surname</th>
-                                <th>Email</th>
-                                <th>Phone</th>
-                                <th>Country</th>
-                                <th>Created at</th>
-                                <th>Updated at</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->surname }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->phone }}</td>
-                                <td>{{ $user->country }}</td>
-                                <td>{{ $user->created_at }}</td>
-                                <td>{{ $user->updated_at }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div class="col-md-6">
+                        <h2>Name</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>{{ $user->name }}</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>Surame</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>{{ $user->surname }}</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>Email</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>{{ $user->email }}</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>Phone</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>{{ $user->phone }}</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>Country</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>{{ $user->country }}</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>Created at</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>{{ $user->created_at }}</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>Updated at</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>{{ $user->updated_at }}</h2>
+                    </div>
                 </div>
             </div>
 
             <div id="menu1" class="tab-pane fade">
                 <h3>Menu 1</h3>
-                <p>Some content in menu 1.</p>
+                <h2>Some content in menu 1.</h2>
             </div>
             <div id="menu2" class="tab-pane fade">
                 <h3>Menu 2</h3>
-                <p>Some content in menu 2.</p>
+                <h2>Some content in menu 2.</h2>
             </div>
         </div>
     </div>
